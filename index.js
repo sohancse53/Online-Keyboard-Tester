e58af053b3bc83d2bb234b5bc1ae8cb68ecd450c
Shoyaif Rahman Sohan <155877885+sohancse53@users.noreply.github.com>
2025-01-12 22:27:18 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,391 @@
+addEventListener("keydown", (e) => {
+   if(e.key=="q"){
+    const q=document.getElementById("q");
+    q.style.backgroundColor="grey";
+    q.style.transition="0.3s";
+   }
+
+   if(e.key=="w"){
+    const w=document.getElementById("w");
+    w.style.backgroundColor="grey";
+    w.style.transition="0.3s";
+   }
+
+   if(e.key=="e"){
+    const e=document.getElementById("e");
+    e.style.backgroundColor="grey";
+    e.style.transition="0.3s";
+   }
+
+   if(e.key=="r"){
+    const r=document.getElementById("r");
+    r.style.backgroundColor="grey";
+    r.style.transition="0.3s";
+   }
+
+   if(e.key=="t"){
+    const t=document.getElementById("t");
+    t.style.backgroundColor="grey";
+    t.style.transition="0.3s";
+   }
+
+   if(e.key=="y"){
+    const y=document.getElementById("y");
+    y.style.backgroundColor="grey";
+    y.style.transition="0.3s";
+   }
+
+   if(e.key=="u"){
+    const u=document.getElementById("u");
+    u.style.backgroundColor="grey";
+    u.style.transition="0.3s";
+   }
+
+   if(e.key=="i"){
+    const w=document.getElementById("i");
+    i.style.backgroundColor="grey";
+    i.style.transition="0.3s";
+   }
+   if(e.key=="o"){
+    const o=document.getElementById("o");
+    o.style.backgroundColor="grey";
+    o.style.transition="0.3s";
+   }
+   if(e.key=="p"){
+    const p=document.getElementById("p");
+    p.style.backgroundColor="grey";
+    p.style.transition="0.3s";
+   }
+
+   if(e.key=="Backspace"){
+    const backspace=document.getElementById("backspace");
+    backspace.style.backgroundColor="grey";
+    backspace.style.transition="0.3s";
+   }
+
+   if(e.key=="a"){
+    const a=document.getElementById("a");
+    a.style.backgroundColor="grey";
+    a.style.transition="0.3s";
+   }
+
+   if(e.key=="s"){
+    const s=document.getElementById("s");
+    s.style.backgroundColor="grey";
+    s.style.transition="0.3s";
+   }
+
+   if(e.key=="d"){
+    const d=document.getElementById("d");
+    d.style.backgroundColor="grey";
+    d.style.transition="0.3s";
+   }
+
+   if(e.key=="f"){
+    const backspace=document.getElementById("f");
+    f.style.backgroundColor="grey";
+    f.style.transition="0.3s";
+   }
+
+   if(e.key=="g"){
+    const g=document.getElementById("g");
+    g.style.backgroundColor="grey";
+    g.style.transition="0.3s";
+   }
+
+   if(e.key=="h"){
+    const h=document.getElementById("h");
+    h.style.backgroundColor="grey";
+    h.style.transition="0.3s";
+   }
+
+   if(e.key=="j"){
+    const j=document.getElementById("j");
+    j.style.backgroundColor="grey";
+    j.style.transition="0.3s";
+   }
+
+   if(e.key=="k"){
+    const backspace=document.getElementById("k");
+    k.style.backgroundColor="grey";
+    k.style.transition="0.3s";
+   }
+
+   if(e.key=="l"){
+    const l=document.getElementById("l");
+    l.style.backgroundColor="grey";
+    l.style.transition="0.3s";
+   }
+   if(e.key=="Enter"){
+    const enter=document.getElementById("enter");
+    enter.style.backgroundColor="grey";
+    enter.style.transition="0.3s";
+   }
+
+   if(e.key=="z"){
+    const z=document.getElementById("z");
+    z.style.backgroundColor="grey";
+    z.style.transition="0.3s";
+   }
+
+   if(e.key=="x"){
+    const x=document.getElementById("x");
+    x.style.backgroundColor="grey";
+    x.style.transition="0.3s";
+   }
+
+
+   if(e.key=="c"){
+    const c=document.getElementById("c");
+    c.style.backgroundColor="grey";
+    c.style.transition="0.3s";
+   }
+
+   if(e.key=="v"){
+    const v=document.getElementById("v");
+    v.style.backgroundColor="grey";
+    v.style.transition="0.3s";
+   }
+
+   if(e.key=="b"){
+    const b=document.getElementById("b");
+    b.style.backgroundColor="grey";
+    b.style.transition="0.3s";
+   }
+
+   if(e.key=="n"){
+    const n=document.getElementById("n");
+    n.style.backgroundColor="grey";
+    n.style.transition="0.3s";
+   }
+
+   if(e.key=="m"){
+    const m=document.getElementById("m");
+    m.style.backgroundColor="grey";
+    m.style.transition="0.3s";
+   }
+
+   if(e.code=="Space"){
+    const space=document.getElementById("space");
+    space.style.backgroundColor="grey";
+    space.style.transition="0.3s";
+   }
+   
+  });
+  
+
+
+
+
+  
+
+
+  addEventListener("keyup", (e) => {
+    if(e.key=="q"){
+     const q=document.getElementById("q");
+     q.style.backgroundColor="#e0e0e0";
+     q.style.transition="0.3s";
+    }
+ 
+    if(e.key=="w"){
+     const w=document.getElementById("w");
+     w.style.backgroundColor="#e0e0e0";
+     w.style.transition="0.3s";
+    }
+ 
+    if(e.key=="e"){
+     const e=document.getElementById("e");
+     e.style.backgroundColor="#e0e0e0";
+     e.style.transition="0.3s";
+    }
+ 
+    if(e.key=="r"){
+     const r=document.getElementById("r");
+     r.style.backgroundColor="#e0e0e0";
+     r.style.transition="0.3s";
+    }
+ 
+    if(e.key=="t"){
+     const t=document.getElementById("t");
+     t.style.backgroundColor="#e0e0e0";
+     t.style.transition="0.3s";
+    }
+ 
+    if(e.key=="y"){
+     const y=document.getElementById("y");
+     y.style.backgroundColor="#e0e0e0";
+     y.style.transition="0.3s";
+    }
+ 
+    if(e.key=="u"){
+     const u=document.getElementById("u");
+     u.style.backgroundColor="#e0e0e0";
+     u.style.transition="0.3s";
+    }
+ 
+    if(e.key=="i"){
+     const w=document.getElementById("i");
+     i.style.backgroundColor="#e0e0e0";
+     i.style.transition="0.3s";
+    }
+    if(e.key=="o"){
+     const o=document.getElementById("o");
+     o.style.backgroundColor="#e0e0e0";
+     o.style.transition="0.3s";
+    }
+    if(e.key=="p"){
+     const p=document.getElementById("p");
+     p.style.backgroundColor="#e0e0e0";
+     p.style.transition="0.3s";
+    }
+ 
+    if(e.key=="Backspace"){
+     const backspace=document.getElementById("backspace");
+     backspace.style.backgroundColor="#e0e0e0";
+     backspace.style.transition="0.3s";
+    }
+ 
+    if(e.key=="a"){
+     const a=document.getElementById("a");
+     a.style.backgroundColor="#e0e0e0";
+     a.style.transition="0.3s";
+    }
+ 
+    if(e.key=="s"){
+     const s=document.getElementById("s");
+     s.style.backgroundColor="#e0e0e0";
+     s.style.transition="0.3s";
+    }
+ 
+    if(e.key=="d"){
+     const d=document.getElementById("d");
+     d.style.backgroundColor="#e0e0e0";
+     d.style.transition="0.3s";
+    }
+ 
+    if(e.key=="f"){
+     const backspace=document.getElementById("f");
+     f.style.backgroundColor="#e0e0e0";
+     f.style.transition="0.3s";
+    }
+ 
+    if(e.key=="g"){
+     const g=document.getElementById("g");
+     g.style.backgroundColor="#e0e0e0";
+     g.style.transition="0.3s";
+    }
+ 
+    if(e.key=="h"){
+     const h=document.getElementById("h");
+     h.style.backgroundColor="#e0e0e0";
+     h.style.transition="0.3s";
+    }
+ 
+    if(e.key=="j"){
+     const j=document.getElementById("j");
+     j.style.backgroundColor="#e0e0e0";
+     j.style.transition="0.3s";
+    }
+ 
+    if(e.key=="k"){
+     const backspace=document.getElementById("k");
+     k.style.backgroundColor="#e0e0e0";
+     k.style.transition="0.3s";
+    }
+ 
+    if(e.key=="l"){
+     const l=document.getElementById("l");
+     l.style.backgroundColor="#e0e0e0";
+     l.style.transition="0.3s";
+    }
+    if(e.key=="Enter"){
+     const enter=document.getElementById("enter");
+     enter.style.backgroundColor="#e0e0e0";
+     enter.style.transition="0.3s";
+    }
+ 
+    if(e.key=="z"){
+     const z=document.getElementById("z");
+     z.style.backgroundColor="#e0e0e0";
+     z.style.transition="0.3s";
+    }
+ 
+    if(e.key=="x"){
+     const x=document.getElementById("x");
+     x.style.backgroundColor="#e0e0e0";
+     x.style.transition="0.3s";
+    }
+ 
+ 
+    if(e.key=="c"){
+     const c=document.getElementById("c");
+     c.style.backgroundColor="#e0e0e0";
+     c.style.transition="0.3s";
+    }
+ 
+    if(e.key=="v"){
+     const v=document.getElementById("v");
+     v.style.backgroundColor="#e0e0e0";
+     v.style.transition="0.3s";
+    }
+ 
+    if(e.key=="b"){
+     const b=document.getElementById("b");
+     b.style.backgroundColor="#e0e0e0";
+     b.style.transition="0.3s";
+    }
+ 
+    if(e.key=="n"){
+     const n=document.getElementById("n");
+     n.style.backgroundColor="#e0e0e0";
+     n.style.transition="0.3s";
+    }
+ 
+    if(e.key=="m"){
+     const m=document.getElementById("m");
+     m.style.backgroundColor="#e0e0e0";
+     m.style.transition="0.3s";
+    }
+ 
+    if(e.code=="Space"){
+     const space=document.getElementById("space");
+     space.style.backgroundColor="#e0e0e0";
+     space.style.transition="0.3s";
+    }
+    
+   });
+
+
+
+
+
+
+   let input = document.getElementById("input");
+const arr = [];
+
+addEventListener("keydown", (text) => {
+    if (text.key === "Backspace") {
+        
+        arr.pop();
+    }  else {
+       
+        arr.push(text.key);
+    }
+
+    input.innerHTML = arr.join(""); // Join array to form a string
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
